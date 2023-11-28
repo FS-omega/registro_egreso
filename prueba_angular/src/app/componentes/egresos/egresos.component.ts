998abd9497c4e6287541f16b66aeb478379e59fc
user.name like REIMOC96 <reinaldo.marcelo.ordonez@cftmail.cl>
2023-11-27 21:18:11 -0300

    commit 4

--- prueba_angular/src/app/componentes/egresos/egresos.component.ts
+++ prueba_angular/src/app/componentes/egresos/egresos.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./egresos.component.css']
 })
 export class EgresosComponent {
+  
 
 }
--- prueba_angular/src/app/servicios/formulario/formulario.service.ts
+++ prueba_angular/src/app/servicios/formulario/formulario.service.ts
@@ -8,16 +8,16 @@ import { environment } from '../../../environments/environment.development'
   providedIn: 'root'
 })
 export class FormularioService {
-  url_backend = environment.url_backend+'/registroForm';
+  url_backend = environment.url_backend+'/usuario';
   constructor(
     private http:HttpClient
   ) { }
 
   crear_registro(datos:any){
-    return this.http.post(`${this.url_backend+'/crear-registro' }`,datos)
+    return this.http.post(`${this.url_backend+'/crear-usuario' }`,datos)
   }
   obtener_registros(){
-    return this.http.get(`${this.url_backend+'/obtener-registros' }`)
+    return this.http.get(`${this.url_backend+'/obtener-usuarios' }`)
 
   }
 }
