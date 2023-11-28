74113491ef231805217b5c73eabf50405dcf7e9e
user.name like REIMOC96 <reinaldo.marcelo.ordonez@cftmail.cl>
2023-11-27 21:58:20 -0300

    R C 10

--- prueba_angular/src/app/servicios/egresos/egresos.service.ts
+++ prueba_angular/src/app/servicios/egresos/egresos.service.ts
@@ -5,12 +5,12 @@ import { environment } from 'src/environments/environment.development';
   providedIn: 'root'
 })
 export class EgresosService {
-    url_backend = environment.url_backend+'/egresos'
+    url_backend = environment.url_backend+'/egreso'
     constructor(
       private http:HttpClient
     ){}
     crear_egreso(datos:any){
-      return this.http.post(`${this.url_backend+'crear-egreso'}`,datos)
+      return this.http.post(`${this.url_backend+'/crear-egreso'}`,datos)
     }
     obtener_egresos (){
       return this.http.get(`${this.url_backend+"/obtener-egresos"}`)
