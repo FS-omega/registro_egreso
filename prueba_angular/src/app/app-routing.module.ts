a7c70fce20fe5cdd6e2891cb23b95a2011b17ede
fcu2134 <facundo.marin.moscol@cftmail.cl>
2023-12-04 15:18:05 -0300

    actu

--- prueba_angular/src/app/app-routing.module.ts
+++ prueba_angular/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormularioComponent } from './componentes/crearUsuario/formulario.component';
 import { EgresosComponent } from './componentes/crearEgresos/egresos.component';
+import { VerUsuariosComponent } from './componentes/ver-usuarios/ver-usuarios.component';
 
 
 const routes: Routes = [
@@ -13,7 +14,8 @@ const routes: Routes = [
     path: 'crearEgresos',
     component: EgresosComponent
   },
-  { path: 'crearUsuario/egresos', component: EgresosComponent }
+  { path: 'crearUsuario/egresos', component: EgresosComponent },
+  {path:'verUsuario',component:VerUsuariosComponent}
 ];
 
 @NgModule({
--- prueba_angular/src/app/componentes/ver-usuarios/ver-usuarios.component.ts
+++ prueba_angular/src/app/componentes/ver-usuarios/ver-usuarios.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-import { FormularioComponent } from '../crearUsuario/formulario.component';
+import { Component, OnInit } from '@angular/core';
 import { FormularioService } from 'app/servicios/crearUsuario/formulario.service';
+
 @Component({
   selector: 'app-ver-usuarios',
   templateUrl: './ver-usuarios.component.html',
   styleUrls: ['./ver-usuarios.component.css']
 })
-export class VerUsuariosComponent {
+export class VerUsuariosComponent implements OnInit {
+  usuario:any;
 
+  constructor(private formularioService: FormularioService) {}
+
+  ngOnInit(): void {
+    this.formularioService.obtener_usuario().subscribe(
+      (data) => {
+        
+        this.usuario = data;
+        console.log('Usuarios obtenidos:', this.usuario);
+      },
+      (error) => {
+       
+        console.error('Error al obtener usuarios', error);
+      }
+    );
+  }
 }
