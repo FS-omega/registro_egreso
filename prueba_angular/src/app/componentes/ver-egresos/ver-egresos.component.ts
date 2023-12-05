5c2435172af70bb0e924ce7eb5dea765efdfd4c3
fcu2134 <facundo.marin.moscol@cftmail.cl>
2023-12-05 04:13:59 -0300

    a

--- prueba_angular/src/app/componentes/ver-egresos/ver-egresos.component.ts
+++ prueba_angular/src/app/componentes/ver-egresos/ver-egresos.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms';
+import { EgresosService } from 'app/servicios/crearEgresos/egresos.service';
 @Component({
   selector: 'app-ver-egresos',
   templateUrl: './ver-egresos.component.html',
   styleUrls: ['./ver-egresos.component.css']
 })
 export class VerEgresosComponent {
+  registros: any[]=[]//aca colocas los parametros que queras mostrar o la wa que quieras validar wazaaaaa
 
+  constructor(
+    private egresosService: EgresosService,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.obteneregreso();
+  }
+
+  obteneregreso() {
+    this.egresosService.obtener_egresos().subscribe()
+}
 }
--- prueba_angular/src/app/servicios/ver-egresos/ver-egresos.service.ts
+++ prueba_angular/src/app/servicios/ver-egresos/ver-egresos.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@angular/core';
 import { EgresosComponent } from 'app/componentes/crearEgresos/egresos.component';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VerEgresosService {
 
-  constructor() { }
+
+  url_backend = environment.url_backend+'/egreso'
+  constructor(
+    private http:HttpClient
+  ){}
+  crear_egreso(datos:any){
+    return this.http.post(`${this.url_backend+'/crear-egreso'}`,datos)
+  }
+  obtener_egresos (){
+    return this.http.get(`${this.url_backend+"/obtener-egresos"}`)
+  
+}
 }
