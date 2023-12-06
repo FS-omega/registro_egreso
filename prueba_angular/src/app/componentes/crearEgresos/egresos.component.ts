8befd57e6493c228ab7fca61cf0a2759f85fdfdf
fcu2134 <facundo.marin.moscol@cftmail.cl>
2023-12-06 01:36:36 -0300

    toy chatin

--- prueba_angular/src/app/componentes/crearEgresos/egresos.component.ts
+++ prueba_angular/src/app/componentes/crearEgresos/egresos.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { EgresosService } from '../../servicios/crearEgresos/egresos.service';
-import { FormBuilder,Validators ,FormGroup } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-egresos',
   templateUrl: './egresos.component.html',
   styleUrls: ['./egresos.component.css']
 })
-export class EgresosComponent implements OnInit {
+export class EgresosComponent  {
  
   egreso_Formulario: FormGroup;
+
   constructor(
     private egresosService: EgresosService,
     private fb: FormBuilder
@@ -21,19 +23,29 @@ export class EgresosComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
-  
-    this.crearEgreso();
-  }
+ 
 
   crearEgreso() {
     if (this.egreso_Formulario.valid) {
       const formData = this.egreso_Formulario.value;
       
-       this.egresosService.crear_egreso(formData).subscribe();
-    } 
+      this.egresosService.crear_egreso(formData).subscribe(
+        (response) => {
+          console.log('Egreso creado con éxito:', response);
+         
+        },
+        (error: HttpErrorResponse) => {
+          console.error('Error al crear el egreso:', error);
+
+          if (error.status === 500) {
+            console.error('Error interno del servidor. Por favor, contacta al soporte.');
+          } else {
+            console.error('Error desconocido. Consulta los detalles del error:', error);
+          }
+        }
+      );
+    } else {
+      console.log("El formulario no es válido.");
+    }
   }
-  }
-
-
-
+}
